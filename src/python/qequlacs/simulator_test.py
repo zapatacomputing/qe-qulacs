cec862c7eab9f543232b4af5b882cc53ff6c5b39
simonwa7 <simonwa7@gmail.com>
2020-05-20 15:45:46 -0400

    Removing unneeded imports

--- src/python/qequlacs/simulator_test.py
+++ src/python/qequlacs/simulator_test.py
@@ -1,11 +1,6 @@
 from .simulator import QulacsSimulator
 import unittest
-import numpy as np
-from zquantum.core.circuit import Circuit
 from zquantum.core.interfaces.backend_test import QuantumSimulatorTests
-from openfermion.ops import QubitOperator, IsingOperator
-from pyquil import Program
-from pyquil.gates import H, CNOT, RX, CZ
 
 class TestQulacs(unittest.TestCase, QuantumSimulatorTests):
 
