ccb04f3b2a26423f707f519f9b77288d74c1df43
simonwa7 <simonwa7@gmail.com>
2020-09-15 15:13:39 -0400

    Reworking get_exact_expectation_values function

--- src/python/qequlacs/simulator.py
+++ src/python/qequlacs/simulator.py
@@ -1,22 +1,35 @@
 import os
+
 # It seems that qulacs has some conflict with pyquil, therefore it needs to be imported before zquantum.core.
 import qulacs
 from pyquil.wavefunction import Wavefunction
 from zquantum.core.interfaces.backend import QuantumSimulator
 from zquantum.core.circuit import save_circuit
-from zquantum.core.measurement import (load_wavefunction, load_expectation_values, sample_from_wavefunction,
-                                        ExpectationValues, Measurements, expectation_values_to_real)
-from zquantum.core.measurement import sample_from_wavefunction
+from zquantum.core.measurement import (
+    load_wavefunction,
+    load_expectation_values,
+    sample_from_wavefunction,
+    ExpectationValues,
+    Measurements,
+    expectation_values_to_real,
+)
+from zquantum.core.measurement import (
+    sample_from_wavefunction,
+    expectation_values_to_real,
+    ExpectationValues,
+)
 import openfermion
-from qeopenfermion import reverse_qubit_order
+from qeopenfermion import reverse_qubit_order, expectation
+import numpy as np
 from .utils import convert_circuit_to_qulacs, qubitop_to_qulacspauli
+
 
 class QulacsSimulator(QuantumSimulator):
     def __init__(self, n_samples=None):
         self.n_samples = n_samples
 
     def run_circuit_and_measure(self, circuit, **kwargs):
-        '''
+        """
         Run a circuit and measure a certain number of bitstrings
 
         Args:
@@ -24,13 +37,13 @@ class QulacsSimulator(QuantumSimulator):
             n_samples (int): the number of bitstrings to sample
         Returns:
             a list of bitstrings (a list of tuples)
-        '''
+        """
         wavefunction = self.get_wavefunction(circuit)
         bitstrings = sample_from_wavefunction(wavefunction, self.n_samples)
         return Measurements(bitstrings)
 
     def get_expectation_values(self, circuit, qubit_operator, **kwargs):
-        if self.n_samples==None:
+        if self.n_samples == None:
             return self.get_exact_expectation_values(circuit, qubit_operator, **kwargs)
         else:
             measurements = self.run_circuit_and_measure(circuit)
@@ -41,7 +54,9 @@ class QulacsSimulator(QuantumSimulator):
 
     def get_exact_expectation_values(self, circuit, qubit_operator, **kwargs):
         if self.n_samples != None:
-            raise Exception("Exact expectation values work only for n_samples equal to None.")
+            raise Exception(
+                "Exact expectation values work only for n_samples equal to None."
+            )
 
         # Qubit operators in openfermion use different index ordering than wavefunction.
         qubit_operator = reverse_qubit_order(qubit_operator)
@@ -49,13 +64,17 @@ class QulacsSimulator(QuantumSimulator):
         expectation_values = []
         n_qubits = openfermion.utils.count_qubits(qubit_operator)
 
-        amplitudes = self.get_wavefunction(circuit).amplitudes
-        for term, coeff in qubit_operator.terms.items():
-            term_op = openfermion.QubitOperator(term, coeff)
-            term_sparse = openfermion.utils.qubit_operator_sparse(term_op, n_qubits)
-            expectation_value = openfermion.utils.expectation(term_sparse, amplitudes)
-            expectation_values.append(expectation_value)
-        return ExpectationValues(expectation_values)
+        wavefunction = self.get_wavefunction(circuit)
+
+        # Pyquil does not support PauliSums with no terms.
+        if len(qubit_operator.terms) == 0:
+            return ExpectationValues(np.zeros((0,)))
+
+        values = []
+
+        for op in qubit_operator:
+            values.append(expectation(op, wavefunction))
+        return expectation_values_to_real(ExpectationValues(np.asarray(values)))
 
     def get_wavefunction(self, circuit):
         qulacs_state = self.get_qulacs_state_from_circuit(circuit)
