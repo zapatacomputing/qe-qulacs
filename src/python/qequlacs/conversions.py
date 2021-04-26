1cf6da390207ed4c45e61fb47400b1343ffcac06
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-04-26 14:53:05 +0200

    Add new circuits -> qulacs converter

---
+++ src/python/qequlacs/conversions.py
@@ -0,0 +1,44 @@
+import qulacs
+from zquantum.core.wip import circuits
+
+
+def _identity(x):
+    return x
+
+
+def _negate(x):
+    return -x
+
+
+def _no_params(*args, **kwargs):
+    raise RuntimeError("This gate isn't parametric, you shouldn't need to map its params")
+
+
+ZQUANTUM_TO_QULACS_GATES = {
+    **{
+        gate_name: (getattr(qulacs.gate, gate_name), _no_params)
+        for gate_name in ["I", "X", "Y", "Z", "H", "S", "T"]
+    },
+    **{
+        gate_name: (getattr(qulacs.gate, gate_name), _negate)
+        for gate_name in ["RX", "RY", "RZ", "RH"]
+    },
+    "PHASE": (qulacs.gate.U1, _identity),
+    # TODO: add two-qubit gates
+    # TODO: add custom gates
+}
+
+
+def _qulacs_gate(operation: circuits.GateOperation):
+    qulacs_gate_factory, param_transform = ZQUANTUM_TO_QULACS_GATES[operation.gate.name]
+    return qulacs_gate_factory(
+        *operation.qubit_indices, *map(param_transform, operation.gate.params)
+    )
+
+
+def convert_to_qulacs(circuit: circuits.Circuit):
+    qulacs_circuit = qulacs.QuantumCircuit(circuit.n_qubits)
+    for operation in circuit.operations:
+        qulacs_circuit.add_gate(_qulacs_gate(operation))
+
+    return qulacs_circuit
