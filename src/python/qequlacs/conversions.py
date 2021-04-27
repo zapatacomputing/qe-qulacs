a065ff318c08b43d291845f577ad9d58fca7298d
Alexander Juda <6004040+alexjuda@users.noreply.github.com>
2021-04-27 11:26:10 +0200

    Migrate backend to new circuits (#25)
    * Fix line lengths
    
    * Remove unused imports
    
    * Add new circuits -> qulacs converter
    
    * Migrate simulator to use new circuits
    
    * Add support for converting 2-qubit & custom gates to qulacs
    
    * Fix converting pauli rotation gates
    
    * Fix converting XY gates to qulacs
    
    * Convert CPHASE gate explicitly
    
    * Remove unused utils module
    
    * Remove unused import

---
+++ src/python/qequlacs/conversions.py
@@ -0,0 +1,98 @@
+import qulacs
+from zquantum.core.wip import circuits
+import numpy as np
+
+
+def _identity(x):
+    return x
+
+
+def _negate(x):
+    return -x
+
+
+def _no_params(*args, **kwargs):
+    raise RuntimeError(
+        "This gate isn't parametric, you shouldn't need to map its params"
+    )
+
+
+def _gate_factory_from_pauli_rotation(axes):
+    def _factory(*args):
+        qubit_indices = args[: len(axes)]
+        params = args[len(axes) :]
+        return qulacs.gate.PauliRotation(qubit_indices, axes, *params)
+
+    return _factory
+
+
+ZQUANTUM_TO_QULACS_GATES = {
+    # 1-qubit, non-parametric
+    "I": (qulacs.gate.Identity, _no_params),
+    **{
+        gate_name: (getattr(qulacs.gate, gate_name), _no_params)
+        for gate_name in ["X", "Y", "Z", "H", "S", "T"]
+    },
+    # 1-qubit, parametric
+    **{
+        gate_name: (getattr(qulacs.gate, gate_name), _negate)
+        for gate_name in ["RX", "RY", "RZ"]
+    },
+    "PHASE": (qulacs.gate.U1, _identity),
+    # 2-qubit, non-parametric
+    **{
+        gate_name: (getattr(qulacs.gate, gate_name), _no_params)
+        for gate_name in ["CNOT", "SWAP"]
+    },
+    # 2-qubit, parametric
+    **{
+        gate_name: (_gate_factory_from_pauli_rotation([ax, ax]), _negate)
+        for ax, gate_name in enumerate(["XX", "YY", "ZZ"], start=1)
+    },
+}
+
+
+def _make_cphase_gate(operation: circuits.GateOperation):
+    matrix = np.diag([1.0, np.exp(1.0j * operation.gate.params[0])])
+    gate_to_add = qulacs.gate.DenseMatrix(operation.qubit_indices[1], matrix)
+    gate_to_add.add_control_qubit(operation.qubit_indices[0], 1)
+    return gate_to_add
+
+
+GATE_SPECIAL_CASES = {
+    "CPHASE": _make_cphase_gate,
+}
+
+
+def _qulacs_gate(operation: circuits.GateOperation):
+    try:
+        factory = GATE_SPECIAL_CASES[operation.gate.name]
+        return factory(operation)
+    except KeyError:
+        pass
+
+    try:
+        qulacs_gate_factory, param_transform = ZQUANTUM_TO_QULACS_GATES[
+            operation.gate.name
+        ]
+        return qulacs_gate_factory(
+            *operation.qubit_indices, *map(param_transform, operation.gate.params)
+        )
+    except KeyError:
+        pass
+
+    return _custom_qulacs_gate(operation)
+
+
+def _custom_qulacs_gate(operation: circuits.GateOperation):
+    matrix = operation.gate.matrix
+    dense_matrix = np.array(matrix, dtype=complex)
+    return qulacs.gate.DenseMatrix(list(operation.qubit_indices), dense_matrix)
+
+
+def convert_to_qulacs(circuit: circuits.Circuit):
+    qulacs_circuit = qulacs.QuantumCircuit(circuit.n_qubits)
+    for operation in circuit.operations:
+        qulacs_circuit.add_gate(_qulacs_gate(operation))
+
+    return qulacs_circuit
--- src/python/qequlacs/simulator.py
+++ src/python/qequlacs/simulator.py
@@ -1,27 +1,22 @@
-import os
-
-# It seems that qulacs has some conflict with pyquil, therefore it needs to be imported before zquantum.core.
+# It seems that qulacs has some conflict with pyquil, therefore it needs to be imported
+# before zquantum.core.
 import qulacs
 import numpy as np
 from qulacs.observable import create_observable_from_openfermion_text
 from pyquil.wavefunction import Wavefunction
 from zquantum.core.interfaces.backend import QuantumSimulator
-from zquantum.core.circuit import save_circuit, Circuit
+from zquantum.core.circuit import Circuit as OldCircuit
 from zquantum.core.measurement import (
-    load_wavefunction,
-    load_expectation_values,
     sample_from_wavefunction,
     ExpectationValues,
     Measurements,
-    expectation_values_to_real,
 )
-from zquantum.core.measurement import (
-    sample_from_wavefunction,
-    expectation_values_to_real,
-    ExpectationValues,
-)
-import openfermion
-from .utils import convert_circuit_to_qulacs, qubitop_to_qulacspauli
+from zquantum.core.wip.circuits import (
+     new_circuit_from_old_circuit,
+     Circuit as NewCircuit,
+ )
+from zquantum.core.wip.compatibility_tools import compatible_with_old_type
+from .conversions import convert_to_qulacs
 from typing import Optional
 
 
@@ -32,8 +27,11 @@ class QulacsSimulator(QuantumSimulator):
     def __init__(self, n_samples=None):
         super().__init__(n_samples)
 
+    @compatible_with_old_type(
+        old_type=OldCircuit, translate_old_to_wip=new_circuit_from_old_circuit
+    )
     def run_circuit_and_measure(
-        self, circuit: Circuit, n_samples: Optional[int] = None, **kwargs
+        self, circuit: NewCircuit, n_samples: Optional[int] = None, **kwargs
     ):
         """
         Run a circuit and measure a certain number of bitstrings
@@ -55,14 +53,20 @@ class QulacsSimulator(QuantumSimulator):
         bitstrings = sample_from_wavefunction(wavefunction, n_samples)
         return Measurements(bitstrings)
 
+    @compatible_with_old_type(
+        old_type=OldCircuit, translate_old_to_wip=new_circuit_from_old_circuit
+    )
     def get_wavefunction(self, circuit):
         super().get_wavefunction(circuit)
         qulacs_state = self.get_qulacs_state_from_circuit(circuit)
         amplitudes = qulacs_state.get_vector()
         return Wavefunction(amplitudes)
 
+    @compatible_with_old_type(
+        old_type=OldCircuit, translate_old_to_wip=new_circuit_from_old_circuit
+    )
     def get_exact_expectation_values(self, circuit, qubit_operator, **kwargs):
-        if self.n_samples != None:
+        if self.n_samples is not None:
             raise Exception(
                 "Exact expectation values work only for n_samples equal to None."
             )
@@ -82,9 +86,11 @@ class QulacsSimulator(QuantumSimulator):
                 )
         return ExpectationValues(np.array(expectation_values))
 
-    def get_qulacs_state_from_circuit(self, circuit):
-        qulacs_circuit = convert_circuit_to_qulacs(circuit)
-        num_qubits = len(circuit.qubits)
-        qulacs_state = qulacs.QuantumState(num_qubits)
+    @compatible_with_old_type(
+        old_type=OldCircuit, translate_old_to_wip=new_circuit_from_old_circuit
+    )
+    def get_qulacs_state_from_circuit(self, circuit: NewCircuit):
+        qulacs_circuit = convert_to_qulacs(circuit)
+        qulacs_state = qulacs.QuantumState(circuit.n_qubits)
         qulacs_circuit.update_quantum_state(qulacs_state)
         return qulacs_state
